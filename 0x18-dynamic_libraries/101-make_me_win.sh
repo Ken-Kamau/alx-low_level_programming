506d53bb08724b6047c27bcde47824c0d6ace2ba
Ken-Kamau <20206809+Ken-Kamau@users.noreply.github.com>
2022-06-13 23:23:58 +0300

    dlibs

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/lroudge/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/Ken-Kamau/alx-low_level_programming/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
